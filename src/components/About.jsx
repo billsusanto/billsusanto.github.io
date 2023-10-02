ef5c7ea09150ec53af412af13c06aa58d5f6640f
billsusanto <billsusanto01@gmail.com>
2023-10-02 16:49:41 -0700

    Ready for V15 deployment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,27 +16,27 @@ const ServiceCard = ({ index, title, icon, back }) => {
   };
 
   return (
-    <Tilt className="flex xs:w-[100px] xs:h-[100px] sm:w-[150px] sm:h-[150px] md:w-[150px] md:h-[200px] lg:w-[250px] lg:h-[300px]">
-      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className='w-full p-[1px] rounded-[20px] shadow-card service-card' onClick={handleClick}>
+    <Tilt className="flex flex-row xs:w-[100px] xs:h-[100px] sm:w-[150px] sm:h-[150px] md:w-[150px] md:h-[200px] lg:w-[250px] lg:h-[300px]">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)} className='min-w-[100px] w-full p-[1px] rounded-[20px] shadow-card service-card' onClick={handleClick}>
         <div className={`flip-card ${flipped ? 'flipped' : ''}`}>
           <div className="flip-card-inner">
             <div className="flip-card-front">
               <motion.div
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
-                className='front-content bg-teal rounded-[30px] py-5 px-12 xs:w-[100px] xs:h-[100px] sm:w-[150px] sm:h-[150px] md:w-[150px] md:h-[150px] lg:w-[250px] lg:h-[250px] flex justify-evenly items-center flex-col'
+                className='front-content bg-teal rounded-[30px] py-5 px-10 xs:w-[120px] xs:h-[120px] sm:w-[150px] sm:h-[150px] md:w-[150px] md:h-[150px] lg:w-[250px] lg:h-[250px] w-[120px] h-[120px] flex justify-evenly items-center flex-col'
               >
-                <img src={icon} alt={title} className='xs:w-[50px] xs:h-[50px] sm:w-[54px] sm:h-[54px] md:w-[100px] md:h-[100px] xl:w-[150px] xl:h-[150px] object-contain' />
-                <h3 className='xs:hidden sm:hidden md:block xl:block text-white text-[20px] font-bold text-center'>{title}</h3>
+                <img src={icon} alt={title} className='xs:w-[50px] xs:h-[50px] sm:w-[54px] sm:h-[54px] md:w-[100px] md:h-[100px] xl:w-[150px] xl:h-[150px] w-[40px] h-[40px] object-contain' />
+                <h3 className='hidden md:block xl:block text-white text-[20px] font-bold text-center'>{title}</h3>
               </motion.div>
             </div>
-            <div className="flip-card-back">
+            <div className="flip-card-back ">
               <motion.div
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
-                className='back-content bg-teal rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+                className='back-content bg-teal rounded-[20px] py-5 px-15 min-h-[180px] flex justify-evenly items-center flex-col xs:text-[20px] sm:text-[25px] md:text-[30px] xl:text-[30px]'
               >
-                <h3 className='text-white text-[15px] font-bold'>{back}</h3>
+                <h3 className='text-white font-bold xs:text-[10px] sm:text-[15px] md:text-[20px] xl:text-[25px] text-[12px]'>{back}</h3>
               </motion.div>
             </div>
           </div>
@@ -51,7 +51,7 @@ const About = () => {
     <div id="About" className="mt-20 mx-auto w-4/5 min-h-screen bg-cover bg-no-repeat bg-center rounded-2xl" style={{ backgroundImage: `url(${armory})` }}>
       <div className="w-full flex flex-col justify-end">
 
-          <h2 className="text-white font-bold text-6xl xs:text-[35px] sm:text-[45px] md:text-[55px] xl:text-[65px]">
+          <h2 className="text-white font-bold text-6xl xs:text-[35px] sm:text-[45px] md:text-[55px] xl:text-[65px] text-[40px]">
             <div className="ml-8 mt-20">My Strengths</div>
           </h2>
       </div>
@@ -63,8 +63,8 @@ const About = () => {
       </div>
 
       <div className="flex justify-center">
-        <div className="mt-36">
-          <div className="flex flex-wrap justify-center gap-10">
+        <div className="mt-0 md:mt-10 xl:mt-25">
+          <div className="flex flex-wrap justify-center gap-20 min-h-[400px] min-w-[200px]">
             {services.map((service, index, back) => (
               <ServiceCard key={service.title} index={index} back={back} {...service} />
             ))}
